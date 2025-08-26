8b71c0109761b22c528f8a9cd3eabe97bbac28b3
jme14 <williamsltrey3@gmail.com>
2025-08-26 13:14:07 -0500

    add getting cast to api call

--- src/app/api/parse/route.ts
+++ src/app/api/parse/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 import parseTreeString, { printPrettyTree } from "@/utils/parseTreeString";
-import { ParseTree } from "@/types/ParseTree";
+import { ParseTree } from "@/types/api/ParseTree";
 import getParseTreeStats from "@/utils/getParseTreeStats";
-import { ParsePostResponse } from "@/types/ParsePostResponse";
-function getParseTreeFromData(data: {
+import { ParsePostResponse } from "@/types/api/ParsePostResponse";
+import getCastFromSentence from "@/utils/game/getCastFromSentence";
+import PlayerUnlocks from "@/types/game/PlayerUnlocks";
+export function getParseTreeFromData(data: {
     sentences: Array<{ parse: string }>;
 }): ParseTree {
     const sentenceResponse = data.sentences[0];
@@ -31,12 +33,20 @@ export async function POST(req: Request) {
 
     const data = await res.json();
     const pt = getParseTreeFromData(data);
+    const cParse = printPrettyTree(pt);
     const ptStats = getParseTreeStats(pt);
+    const spellKey = {
+        ptStats: ptStats,
+        cParse: cParse,
+        unlocks: new PlayerUnlocks(),
+    };
+    const playerCast = getCastFromSentence(spellKey);
 
     const postResponse: ParsePostResponse = {
         parseTree: pt,
         constituencyParse: printPrettyTree(pt),
         parseTreeStats: ptStats,
+        playerCast: playerCast,
     };
     return NextResponse.json(postResponse);
 }
