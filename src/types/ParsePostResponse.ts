26888f1fe94f6ab9382a1d632545eefb3762f317
jme14 <williamsltrey3@gmail.com>
2025-08-29 14:19:15 -0500

    formatting

--- src/types/ParsePostResponse.ts
+++
@@ -1,7 +0,0 @@
-import { ParseTree } from "./ParseTree";
-import ParseTreeStats from "./ParseTreeStats";
-export interface ParsePostResponse {
-    parseTree: ParseTree;
-    constituencyParse: string[];
-    parseTreeStats: ParseTreeStats;
-}
