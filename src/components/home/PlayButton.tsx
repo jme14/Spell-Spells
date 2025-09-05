a59aa4f323c8fc6a7c81eed9fb6b22161551b7f6
jme14 <williamsltrey3@gmail.com>
2025-09-05 14:48:58 -0500

    add component for play button

---
+++ src/components/home/PlayButton.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+interface PlayButtonProps {
+    className: string;
+}
+export default function PlayButton({ className }: PlayButtonProps) {
+    return (
+        <Link href="/waiting">
+            <button
+                className={`rounded bg-blue-600 text-white hover:bg-blue-700 ${className}`}
+                onClick={() => console.log("Clicked!")}
+            >
+                Play
+            </button>
+        </Link>
+    );
+}
