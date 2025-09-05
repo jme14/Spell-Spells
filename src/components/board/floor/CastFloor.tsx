e184ae5a828217c92ae0d8901ce53bdc3ce33d41
jme14 <williamsltrey3@gmail.com>
2025-09-05 10:50:17 -0500

    add parameter to flip row1 and row2

--- src/components/board/floor/CastFloor.tsx
+++ src/components/board/floor/CastFloor.tsx
@@ -1,10 +1,18 @@
-import { PropsWithChildren } from "react";
+import React, { PropsWithChildren } from "react";
 interface CastFloorProps {
     className: string;
+    rowReverse: boolean;
 }
 export default function CastFloor({
     className,
     children,
+    rowReverse = false,
 }: PropsWithChildren<CastFloorProps>) {
-    return <div className={`grid grid-cols-2 ${className}`}>{children}</div>;
+    const childrenAsArray = React.Children.toArray(children);
+    const flippedChildren = rowReverse
+        ? [...childrenAsArray.slice(2), ...childrenAsArray.slice(0, 2)]
+        : childrenAsArray;
+    return (
+        <div className={`grid grid-cols-2 ${className}`}>{flippedChildren}</div>
+    );
 }
