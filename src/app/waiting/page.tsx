17eb8085bc3fa31dfbb846c80e257608e563a857
jme14 <williamsltrey3@gmail.com>
2025-09-05 10:48:45 -0500

    first commit waiting page

---
+++ src/app/waiting/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { socket } from "@/lib/socket";
+import { useAppDispatch } from "@/hooks/hooks";
+import {
+    setGameId,
+    markPlayer2,
+    flipTurn,
+} from "@/state/slices/player/playerSlice";
+
+interface MatchFoundResponse {
+    gameId: string;
+    isPlayerOne: boolean;
+}
+
+export default function Waiting() {
+    const dispatch = useAppDispatch();
+    const router = useRouter();
+
+    useEffect(() => {
+        socket.emit("waiting");
+    });
+    useEffect(() => {
+        const onMatchFound = ({ gameId, isPlayerOne }: MatchFoundResponse) => {
+            dispatch(setGameId(gameId));
+
+            // if player2, mark them as such
+            if (isPlayerOne === false) {
+                dispatch(markPlayer2());
+                // otherwise, mark it as their turn
+            } else {
+                dispatch(flipTurn());
+            }
+
+            router.push("/");
+        };
+        socket.on("matchFound", onMatchFound);
+        return () => {
+            socket.off("matchFound", onMatchFound);
+        };
+    }, [router, dispatch]);
+    return <div>You are in the waiting room!</div>;
+}
