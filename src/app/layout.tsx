353a769d9f6a583982a02a6883aafa5d2988b37d
jme14 <williamsltrey3@gmail.com>
2025-09-05 10:49:19 -0500

    add redux provider; change imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import ReduxProvider from "@/components/misc/ReduxProvider";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <ReduxProvider> {children}</ReduxProvider>
+            </body>
         </html>
     );
 }
