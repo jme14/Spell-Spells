67e9090a22a0f2c82e89a51cbb6cb1d8c110214c
jme14 <williamsltrey3@gmail.com>
2025-09-09 13:17:21 -0500

    change sentence behavior to be in imported hooks

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -1,14 +1,4 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
-import { socket } from "@/lib/socket";
-
-import {
-    ParseTree,
-    ParseTreeStats,
-    ParsePostResponse,
-} from "spell-spells-schema";
-
 import PosTable from "@/components/board/pos-table/PosTable";
 import SpellBook from "@/components/board/spellBook/SpellBook";
 import PlayerCast from "@/components/board/floor/PlayerCast";
@@ -18,24 +8,15 @@ import HealthOrb from "@/components/board/HealthOrb";
 import UsedWords from "@/components/board/UsedWords";
 import ConstituencyTreeLeaf from "@/components/board/tree/ConstituencyTree";
 import LetterBank from "@/components/board/LetterBank";
-import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
 
-import {
-    newSpellSlotA,
-    newSpellSlotB,
-    newSpellSlotC,
-    newSpellSlotD,
-} from "@/state/slices/playerCasts/playerCastsSlice";
-import { flipTurn } from "@/state/slices/player/playerSlice";
+import { useAppSelector } from "@/hooks/hooks";
+import { useSubmitSentence } from "@/hooks/useSubmitSentence";
+import { useOnSentenceSubmit } from "@/hooks/useOnSentenceSubmit";
 
 export default function Home() {
-    const hasMounted = useRef(false);
-    const router = useRouter();
-
-    const dispatch = useAppDispatch();
-
+    // from global state
     const { player1, myTurn } = useAppSelector((state) => state.player.value);
-    const sentence = useAppSelector((state) => state.sentence.value);
+    const { sentenceRecieved } = useAppSelector((state) => state.sentence);
 
     const { p1Health, p2Health } = useAppSelector(
         (state) => state.playerHealths.value
@@ -44,67 +25,10 @@ export default function Home() {
         (state) => state.playerCasts.value
     );
 
-    const [tree, setTree] = useState<ParseTree>({ None: "None" });
-    const [posTable, setPosTable] = useState<ParseTreeStats | null>(null);
-
-    /*
-    // on first load
-    useEffect(() => {
-        socket.on("gameStart", () => {
-            // do stuff on gameStart action
-        });
-    }, []);
-    */
-
-    // on sentence change
-    useEffect(() => {
-        if (!hasMounted.current) {
-            hasMounted.current = true;
-            return;
-        }
-        if (sentence === "Type a sentence to cast a spell.") {
-            return;
-        }
-        socket.emit("sentenceSubmit", { sentence: sentence });
-    }, [sentence]);
-
-    // defining what takes place on sentenceSubmit and opponentDisconnect
-    useEffect(() => {
-        if (!hasMounted.current) {
-            hasMounted.current = true;
-            return;
-        }
-        const onSentenceSubmitResponse = ({
-            parseTreeStats,
-            parseTree,
-            playerCast,
-            playerCastLocation,
-        }: ParsePostResponse) => {
-            console.log("Obtained a response from the server.");
-            setPosTable(parseTreeStats);
-            setTree(parseTree);
-
-            if (playerCastLocation === "A") {
-                dispatch(newSpellSlotA(playerCast));
-            } else if (playerCastLocation === "B") {
-                dispatch(newSpellSlotB(playerCast));
-            } else if (playerCastLocation === "C") {
-                dispatch(newSpellSlotC(playerCast));
-            } else if (playerCastLocation === "D") {
-                dispatch(newSpellSlotD(playerCast));
-            }
-
-            dispatch(flipTurn());
-        };
-        socket.on("sentenceSubmitResponse", onSentenceSubmitResponse);
-        socket.on("opponentDisconnect", () => {
-            dispatch({ type: "RESET" });
-            router.push("/waiting");
-        });
-        return () => {
-            socket.off("sentenceSubmitResponse", onSentenceSubmitResponse);
-        };
-    }, [dispatch, router]);
+    // logic for how to handle recieving a new sentence submission
+    const { tree, posTable } = useOnSentenceSubmit();
+    // logic for what happens upon a sentence being submitted
+    useSubmitSentence();
 
     return (
         <div className={`flex justify-center items-center`}>
@@ -130,7 +54,7 @@ export default function Home() {
                         id="col2"
                         className={`col-span-8 grid grid-rows-[1fr_7fr_1fr] min-h-[750px]`}
                     >
-                        <div>{sentence}</div>
+                        <div>{sentenceRecieved}</div>
                         <CastFloor
                             className={`border min-h-[100px]`}
                             rowReverse={!player1}
