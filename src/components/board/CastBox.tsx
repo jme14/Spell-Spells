ab19f9f6f783825a4df2d03879844d11a78b592d
jme14 <williamsltrey3@gmail.com>
2025-09-09 13:18:00 -0500

    modify submitSentence state on submission

--- src/components/board/CastBox.tsx
+++ src/components/board/CastBox.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAppSelector, useAppDispatch } from "@/hooks/hooks";
-import { newSentence } from "@/state/slices/sentence/sentenceSlice";
+import { submitSentence } from "@/state/slices/sentence/sentenceSlice";
 
 interface CastBoxProps {
     className: string;
@@ -8,11 +8,11 @@ interface CastBoxProps {
 export function CastBox({ className }: CastBoxProps) {
     const dispatch = useAppDispatch();
     const { myTurn } = useAppSelector((state) => state.player.value);
-    const [sentence, setSentence] = useState("");
+    const [sentenceLocal, setSentenceLocal] = useState("");
 
     function onEnterKey(e: React.KeyboardEvent<HTMLInputElement>) {
         if (e.key === "Enter" && myTurn) {
-            dispatch(newSentence(sentence));
+            dispatch(submitSentence(sentenceLocal));
         }
     }
 
@@ -20,8 +20,8 @@ export function CastBox({ className }: CastBoxProps) {
         <input
             className={`border  ${className}`}
             type="text"
-            value={sentence}
-            onChange={(e) => setSentence(e.target.value)}
+            value={sentenceLocal}
+            onChange={(e) => setSentenceLocal(e.target.value)}
             onKeyDown={onEnterKey}
         />
     );
