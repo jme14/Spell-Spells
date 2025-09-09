30b55d0dc18d9aefd984148f459242495da76277
jme14 <williamsltrey3@gmail.com>
2025-09-09 13:20:29 -0500

    use createSelector for improved performance

--- src/state/selectors/selectGameState.ts
+++ src/state/selectors/selectGameState.ts
@@ -1,14 +1,31 @@
 import { RootState } from "@/state/store";
 
-export function selectGameState(state: RootState) {
-    return {
-        sentence: state.sentence,
-        playerCasts: state.playerCasts,
-        playerHealths: state.playerHealths,
-        // tree
-        // lastSentGenPos
-        usedWords: state.usedWords,
-        letterBank: state.letterBank,
-        player: state.player,
-    };
-}
+import { createSelector } from "@reduxjs/toolkit";
+
+const selectSentence = (state: RootState) => state.sentence;
+const selectPlayerCasts = (state: RootState) => state.playerCasts;
+const selectPlayerHealths = (state: RootState) => state.playerHealths;
+const selectUsedWords = (state: RootState) => state.usedWords;
+const selectLetterBank = (state: RootState) => state.letterBank;
+const selectPlayer = (state: RootState) => state.player;
+
+export const selectGameState = createSelector(
+    [
+        selectSentence,
+        selectPlayerCasts,
+        selectPlayerHealths,
+        selectUsedWords,
+        selectLetterBank,
+        selectPlayer,
+    ],
+    (sentence, playerCasts, playerHealths, usedWords, letterBank, player) => {
+        return {
+            sentence: sentence,
+            playerCasts: playerCasts,
+            playerHealths: playerHealths,
+            usedWords: usedWords,
+            letterBank: letterBank,
+            player: player,
+        };
+    }
+);
