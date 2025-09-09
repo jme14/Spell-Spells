493abb143a276804a3d6e74c7b6f51e3e5bb7455
jme14 <williamsltrey3@gmail.com>
2025-09-09 16:03:32 -0500

    on home page, require loading of connection to server before pressing play

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 
 import PlayButton from "@/components/home/PlayButton";
 
+import { useInitServerConnection } from "@/hooks/useInitServerConnection";
+
 export default function Home() {
+    const { clientConnected } = useInitServerConnection();
+    if (!clientConnected) {
+        return <div>Loading...</div>;
+    }
+
+    // if the client is connected, then we can press play.
     return (
         <div className="flex flex-col items-center h-screen w-screen">
             <div className="">
