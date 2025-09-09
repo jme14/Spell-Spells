8fbfb4cc5a52ab4dbfc017785b1fdb8420e36813
jme14 <williamsltrey3@gmail.com>
2025-09-09 16:04:32 -0500

    refactor to create function for initializing socket and function for getting the socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,3 +1,32 @@
-import { io } from "socket.io-client";
+import { Socket, io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000");
+let socket: Socket | null = null;
+let socketPromise: Promise<Socket> | null = null;
+
+export async function initSocket(): Promise<Socket> {
+    if (socket) return socket;
+    if (socketPromise) return socketPromise;
+
+    socketPromise = (async () => {
+        await fetch("http://localhost:4000/init", {
+            method: "GET",
+            credentials: "include",
+        });
+
+        socket = io("http://localhost:4000", { withCredentials: true });
+
+        console.log("Here is the socket now that is has been initialized");
+        console.log(socket);
+
+        return socket;
+    })();
+    return socketPromise;
+}
+export function getSocket() {
+    if (!socket) {
+        throw new Error("Socket not initialized, call initSocket");
+    }
+    console.log("Get socket is returning the following");
+    console.log(socket);
+    return socket;
+}
