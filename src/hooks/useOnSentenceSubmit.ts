0e280c65942ef566aef3e439a995c3302fc55cff
jme14 <williamsltrey3@gmail.com>
2025-09-09 13:18:37 -0500

    refactor sentence submit/recieve logic to these hooks

---
+++ src/hooks/useOnSentenceSubmit.ts
@@ -0,0 +1,86 @@
+import { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+    ParseTree,
+    ParseTreeStats,
+    GameStateServer,
+} from "spell-spells-schema";
+
+import { socket } from "@/lib/socket";
+import { useAppDispatch } from "@/hooks/hooks";
+import {
+    newSpellSlotA,
+    newSpellSlotB,
+    newSpellSlotC,
+    newSpellSlotD,
+} from "@/state/slices/playerCasts/playerCastsSlice";
+import { recieveSentence } from "@/state/slices/sentence/sentenceSlice";
+import { flipTurn } from "@/state/slices/player/playerSlice";
+
+export function useOnSentenceSubmit() {
+    const hasMounted = useRef(false);
+    const { push } = useRouter();
+
+    const dispatch = useAppDispatch();
+
+    const [tree, setTree] = useState<ParseTree>({ None: "None" });
+    const [posTable, setPosTable] = useState<ParseTreeStats | null>(null);
+
+    // defining what takes place on sentenceSubmit and opponentDisconnect
+    useEffect(() => {
+        if (!hasMounted.current) {
+            hasMounted.current = true;
+            return;
+        }
+        const onSentenceSubmitResponse = ({
+            // for updating UI
+            parseTree,
+            parseTreeStats,
+
+            // for updating state
+            lastSentence,
+            playerCast,
+        }: /*
+            playerCast: PlayerCast;
+            playerOneHealth: number;
+            playerTwoHealth: number;
+            usedWords: string[];
+            letterBank: string[];
+            */
+        GameStateServer) => {
+            setPosTable(parseTreeStats);
+            setTree(parseTree);
+
+            // updatingSentence happens here
+            dispatch(recieveSentence(lastSentence));
+
+            // updating playerCast state
+            const { playerSpell, playerCastLocation } = playerCast;
+            if (playerCastLocation === "A") {
+                dispatch(newSpellSlotA(playerSpell));
+            } else if (playerCastLocation === "B") {
+                dispatch(newSpellSlotB(playerSpell));
+            } else if (playerCastLocation === "C") {
+                dispatch(newSpellSlotC(playerSpell));
+            } else if (playerCastLocation === "D") {
+                dispatch(newSpellSlotD(playerSpell));
+            }
+
+            // changing turn
+            dispatch(flipTurn());
+        };
+        const onOpponentDisconnect = () => {
+            dispatch({ type: "RESET" });
+            push("/waiting");
+        };
+        socket.on("sentenceSubmitResponse", onSentenceSubmitResponse);
+        socket.on("opponentDisconnect", onOpponentDisconnect);
+        return () => {
+            socket.off("sentenceSubmitResponse", onSentenceSubmitResponse);
+            socket.off("opponentDisconnect", onOpponentDisconnect);
+        };
+    }, [dispatch, push]);
+
+    return { tree, posTable };
+}
---
+++ src/hooks/useSubmitSentence.ts
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+
+import { socket } from "@/lib/socket";
+import { useAppSelector } from "@/hooks/hooks";
+import { selectGameState } from "@/state/selectors/selectGameState";
+import { GameStateClient } from "spell-spells-schema";
+
+export function useSubmitSentence() {
+    const { sentenceSent, sentenceRecieved } = useAppSelector(
+        (state) => state.sentence
+    );
+    const gameState = useAppSelector(selectGameState);
+
+    useEffect(() => {
+        // nothing has happened; don't send a message
+        if (sentenceSent === sentenceRecieved) {
+            console.log("SentenceSent | Last Submitted");
+            console.log(
+                `Sentence Sent: ${sentenceSent} | Sentence Recieved: ${sentenceRecieved}`
+            );
+            console.log("I guess I'm returnnng");
+            return;
+        }
+
+        const gameStateMessage: GameStateClient = {
+            sentenceSubmission: sentenceSent,
+            playerOneHealth: gameState.playerHealths.value.p1Health,
+            playerTwoHealth: gameState.playerHealths.value.p2Health,
+            usedWords: gameState.usedWords.value,
+            letterBank: gameState.letterBank.value,
+        };
+        console.log("Yup, now I'm emitting");
+        socket.emit("sentenceSubmit", gameStateMessage);
+
+        // console.log("I should have just emitted an event...");
+    }, [sentenceSent, sentenceRecieved, gameState]);
+}
